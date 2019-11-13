c7e0c15d07286b8eb05eec9336b6b95d2919d166
Tamas Gal <tgal@km3net.de>
2019-11-13 21:51:52 +0100

    Initial commit

--- src/ROOTIO.jl
+++ src/ROOTIO.jl
@@ -1,5 +1,21 @@
 module ROOTIO
 
-greet() = print("Hello World!")
+using StaticArrays
+
+struct TKey{T<:Integer}
+    identifier::SVector{4, UInt8}  # Root file identifier ("root")
+    fVersion::Int32               # File format version
+    fBEGIN::Int32                 # Pointer to first data record
+    fEND::T                       # Pointer to first free word at the EOF
+    fSeekFree::T                  # Pointer to FREE data record
+    fNbytesFree::Int32            # Number of bytes in FREE data record
+    nfree::Int32                  # Number of free data records
+    fNbytesName::Int32            # Number of bytes in TNamed at creation time
+    fUnits::UInt8                 # Number of bytes for file pointers
+    fCompress::Int32              # Compression level and algorithm
+    fSeekInfo::T                  # Pointer to TStreamerInfo record
+    fNbytesInfo::Int32            # Number of bytes in TStreamerInfo record
+    fUUID::SVector{18, UInt8}      # Universal Unique ID
+end
 
 end # module
