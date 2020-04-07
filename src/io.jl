5d962e51cb10217082b6c0b9098acc1bc6017fa6
Tamas Gal <tgal@km3net.de>
2020-04-07 13:01:27 +0200

    Add TKey readout

--- src/io.jl
+++ src/io.jl
@@ -2,6 +2,7 @@ function unpack() end
 
 macro io(data)
    struct_name = data.args[2]
+
    types = []
    for f in data.args[3].args
        isa(f, LineNumberNode) && continue
@@ -9,9 +10,30 @@ macro io(data)
        Meta.isexpr(f, :(::)) || error("")
        push!(types, f.args[2])
    end
+
+   struct_size = 0
+   parsers = []
+   for t in types
+       type_ = eval(t)
+       if type_ <: Union{Integer, AbstractFloat}
+           push!(parsers, :(ntoh(read(io, $t))))
+       elseif type_ <: AbstractVector{UInt8}
+           push!(parsers, :(read(io, sizeof($t))))
+       else
+           error("No parser found for type $t")
+       end
+       struct_size += sizeof(type_)
+   end
+
+
    quote
-       $(esc(data))
-       $(@__MODULE__).unpack(io, ::Type{$(esc(struct_name))}) = $(esc(struct_name))($([:(ntoh(read(io, $(esc(t))))) for t in types]...))
-       nothing
+       $(esc(data))  # executing the code to create the actual struct
+       Base.sizeof(::Type{$(esc(struct_name))}) = $struct_size
+
+       function $(@__MODULE__).unpack(io, ::Type{$(esc(struct_name))})
+           $(esc(struct_name))($(parsers...))
+       end
+
+       nothing  # supress REPL output
    end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,40 @@
 using ROOTIO
+using StaticArrays
 using Test
+
 
 @testset "io" begin
     ROOTIO.@io struct Foo
         a::Int32
         b::Int64
         c::Float32
+        d::SVector{5, UInt8}
     end
 
-    foo = Foo(1, 2, 3)
+    d = SA{UInt8}[1, 2, 3, 4, 5]
 
-    @assert foo.a == 1
-    @assert foo.b == 2
-    @assert foo.c ≈ 3
+    foo = Foo(1, 2, 3, d)
+
+    @test foo.a == 1
+    @test foo.b == 2
+    @test foo.c ≈ 3
+    @test d == foo.d
+
+    @test 21 == sizeof(Foo)
+
+    buf = IOBuffer(Vector{UInt8}(1:sizeof(Foo)))
+    foo = ROOTIO.unpack(buf, Foo)
+
+    @test foo.a == 16909060
+    @test foo.b == 361984551142689548
+    @test foo.c ≈ 4.377526f-31
+    @test foo.d == UInt8[0x11, 0x12, 0x13, 0x14, 0x15]
+end
+
+
+@testset "TKey" begin
+    fobj = open(joinpath("test", "samples", "raw.root"))
+    tkey = ROOTIO.unpack(fobj, ROOTIO.TKey)
+    @test "root" == String(tkey.identifier)
+    println(tkey)
 end
