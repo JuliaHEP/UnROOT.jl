410ff5e8615cd0ab8cf44390f6f2ca640ddea60b
Tamas Gal <tgal@km3net.de>
2020-04-19 15:00:46 +0200

    yapf

--- test/samples/tree_with_histos.py
+++ test/samples/tree_with_histos.py
@@ -7,23 +7,23 @@ Creates a simple ROOT file with a tree containing histograms.
 """
 from ROOT import TFile, TTree, TH1F
 from array import array
- 
-h = TH1F( 'h1', 'test', 100, -10., 10. )
- 
-f = TFile( 'tree_with_histos.root', 'recreate' )
-t = TTree( 't1', 'tree with histos' )
- 
+
+h = TH1F('h1', 'test', 100, -10., 10.)
+
+f = TFile('tree_with_histos.root', 'recreate')
+t = TTree('t1', 'tree with histos')
+
 maxn = 10
-n = array( 'i', [ 0 ] )
-d = array( 'f', maxn*[ 0. ] )
-t.Branch( 'mynum', n, 'mynum/I' )
-t.Branch( 'myval', d, 'myval[mynum]/F' )
- 
+n = array('i', [0])
+d = array('f', maxn * [0.])
+t.Branch('mynum', n, 'mynum/I')
+t.Branch('myval', d, 'myval[mynum]/F')
+
 for i in range(25):
-   n[0] = min(i,maxn)
-   for j in range(n[0]):
-      d[j] = i*0.1+j
-   t.Fill()
- 
+    n[0] = min(i, maxn)
+    for j in range(n[0]):
+        d[j] = i * 0.1 + j
+    t.Fill()
+
 f.Write()
 f.Close()
