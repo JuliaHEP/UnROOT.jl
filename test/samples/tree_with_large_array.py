4c2d74bb91bfc7cb0e05c13a9304f627fc6c25e2
Tamas Gal <tgal@km3net.de>
2020-05-01 17:59:34 +0200

    Extend sample

--- test/samples/tree_with_large_array.py
+++ test/samples/tree_with_large_array.py
@@ -12,10 +12,13 @@ f = TFile('tree_with_large_array.root', 'recreate')
 t = TTree('t1', 'tree with large array')
 
 n = array('i', [0])
-t.Branch('large_array', n, 'large_array/I')
+t.Branch('int32_array', n, 'large_array/I')
+m = array('f', [0])
+t.Branch('float_array', m, 'large_array/F')
 
 for i in range(100_000):
     n[0] = i
+    m[0] = i + i / 17
     t.Fill()
 
 f.Write()
