cb8654b7f2ecfc498d61919a87a48129e97bce1e
Tamas Gal <tgal@km3net.de>
2020-05-06 15:21:33 +0200

    Array of Arrays and other preliminary stuff

--- src/ROOTIO.jl
+++ src/ROOTIO.jl
@@ -2,11 +2,13 @@ module ROOTIO
 
 export ROOTFile, array
 
-import Base: keys, get, getindex, show, length, iterate
-using StaticArrays
+import Base: keys, get, getindex, show, length, iterate, sizeof
+
 using CodecZlib
 using Mixers
 using Parameters
+using StaticArrays
+using ArraysOfArrays
 
 include("constants.jl")
 include("utils.jl")
@@ -14,5 +16,6 @@ include("io.jl")
 include("types.jl")
 include("streamers.jl")
 include("root.jl")
+include("custom.jl")
 
 end # module
