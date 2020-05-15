81361448fbc0c0644d0a053a6e0f40853390ce97
Tamas Gal <tgal@km3net.de>
2020-05-15 20:16:37 +0200

    Add KM3NeT event header

--- src/custom.jl
+++ src/custom.jl
@@ -17,3 +17,33 @@ Base.sizeof(T::Type{KM3NETDAQHit}) = 10# Julia 1.0 not supported (fieldtypes?)
 function readtype(io::IO, T::Type{KM3NETDAQHit})
     T(readtype(io, Int32), read(io, UInt8), read(io, Int32), read(io, UInt8))
 end
+
+
+struct KM3NETDAQEventHeader
+    detector_id::Int32
+    run::Int32
+    frame_index::Int32
+    UTC_seconds::UInt32
+    UTC_16nanosecondcycles::UInt32
+    trigger_counter::UInt64
+    trigger_mask::UInt64
+    overlays::UInt32
+end
+
+Base.sizeof(T::Type{KM3NETDAQEventHeader}) = 40
+
+function readtype(io::IO, T::Type{KM3NETDAQEventHeader})
+    skip(io, 18)
+    detector_id = readtype(io, Int32)
+    run = readtype(io, Int32)
+    frame_index = readtype(io, Int32)
+    skip(io, 6)
+    UTC_seconds = readtype(io, UInt32)
+    UTC_16nanosecondcycles = readtype(io, UInt32)
+    skip(io, 6)
+    trigger_counter = readtype(io, UInt64)
+    skip(io, 6)
+    trigger_mask = readtype(io, UInt64)
+    overlays = readtype(io, UInt32)
+    T(detector_id, run, frame_index, UTC_seconds, UTC_16nanosecondcycles, trigger_counter, trigger_mask, overlays)
+end
