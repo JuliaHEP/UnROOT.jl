63d2e9819f9e260eb6a3221df8eeb3d4d808aaa4
Jerry Ling <proton@jling.dev>
2021-07-09 12:13:45 +0200

    Update precompile [skip ci]

--- test/precompile_script.jl
+++ test/precompile_script.jl
@@ -5,18 +5,16 @@ inf_timing = @snoopi tmin=0.01 begin
     a = ROOTFile("$HERE/samples/NanoAODv5_sample.root")
     b = a["Events"]["Electron_dxy"]
     lb = a["Events/Electron_dxy"]
-    tb = Table(a, "Events", ["Electron_dxy"])
-
+    tb = Table(a, "Events")
     @show a,b,lb,tb
-    lb[1:3]
     tb[1:3]
-    tb.Electron_dxy
+    tb.Electron_dxy[1]
+    tb[1].Electron_dxy[1]
 
-    for i in lb
-        i
-    end
     for i in tb
-        i
+        for n in propertynames(tb)
+            getproperty(i, n)
+        end
         break
     end
 
