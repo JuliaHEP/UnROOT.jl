b988dcde778ca263a864df9ffb1b52b91f7474ab
Moelf <proton@jling.dev>
2021-09-05 22:04:50 -0400

    @batch for enumerate()

--- src/polyester.jl
+++ src/polyester.jl
@@ -1,2 +1,5 @@
-Polyester.splitloop(x::LazyTree) = Polyester.NoLoop(), eachindex(x), x
+Polyester.splitloop(t::LazyTree) = Polyester.NoLoop(), eachindex(t), t
 Polyester.combine(t::LazyTree, ::Polyester.NoLoop, j) = LazyEvent(innertable(t), j)
+
+Polyester.splitloop(e::Base.Iterators.Enumerate{LazyTree{T}}) where T = Polyester.NoLoop(), eachindex(e.itr), e
+Polyester.combine(e::Iterators.Enumerate{LazyTree{T}}, ::Polyester.NoLoop, j) where T =  @inbounds e[j]
