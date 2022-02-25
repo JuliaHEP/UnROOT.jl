692adabdb896dff7e0b64ceee4faec82bf460aa7
Jerry Ling <proton@jling.dev>
2022-02-25 14:05:21 -0500

    scitoken ready

--- src/streamsource.jl
+++ src/streamsource.jl
@@ -30,14 +30,39 @@ function Base.close(fobj::MmapStream) # no-op
     nothing
 end
 
+# SciToken discovery https://zenodo.org/record/3937438
+function _find_scitoken()
+    op1 = get(ENV, "BEARER_TOKEN", "")
+    op2 = get(ENV, "BEARER_TOKEN_FILE", "")
+    op3 = get(ENV, "XDG_RUNTIME_DIR", "")
+    uid = Int(Libc.getuid())
+    op3_file = joinpath(op3, "bt_u$uid")
+    op4_file = "/tmp/bt_u$uid"
+    token = if !isempty(op1)
+        op1
+    elseif !isempty(op2)
+        read(op2, String)
+    elseif !isempty(op3) && isfile(op3_file)
+        read(op3_file, String)
+    elseif isfile(op4_file)
+        read(op4_file, String)
+    else
+        ""
+    end
+    return strip(token)
+end
+
 mutable struct HTTPStream
     uri::HTTP.URI
     seekloc::Int
     size::Int
     multipart::Bool
-    function HTTPStream(uri::AbstractString)
+    scitoken::String
+    function HTTPStream(uri::AbstractString; scitoken = _find_scitoken())
         #TODO: determin multipart support
-        test = HTTP.request("GET", uri, ("Range" => "bytes=0-3", "User-Agent" => "UnROOTjl"))
+        test = HTTP.request("GET", uri, 
+        ("Range" => "bytes=0-3", "User-Agent" => "UnROOTjl", "Authorization" => "Bearer $scitoken")
+        )
         @assert test.status==206 "bad network or wrong server"
         @assert String(test.body)=="root" "not a root file"
         multipart = false
@@ -49,7 +74,7 @@ mutable struct HTTPStream
             end
         end
         size = parse(Int, match(r"/(\d+)", v).captures[1])
-        new(HTTP.URI(uri), 0, size, multipart)
+        new(HTTP.URI(uri), 0, size, multipart, scitoken)
     end
 end
 
@@ -91,7 +116,7 @@ end
 
 function read_seek_nb(fobj::HTTPStream, seek, nb)
     stop = seek+nb-1
-    hd = ("Range" => "bytes=$(seek)-$stop",)
+    hd = ("Range" => "bytes=$(seek)-$stop", "Authorization" => "Bearer $(fobj.scitoken)")
     b = HTTP.request(HTTP.stack(), "GET", fobj.uri, hd, UInt8[]).body
     return b
 end
