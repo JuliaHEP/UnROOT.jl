0908ff5df6fb2e86d7952141878ed908f1d8a729
Jerry Ling <proton@jling.dev>
2022-03-02 00:50:51 -0500

    backport upgrade

--- src/streamsource.jl
+++ src/streamsource.jl
@@ -1,11 +1,4 @@
-using xrootdgo_jll
 import HTTP
-
-mutable struct XRDStream
-    gofile_id::Cstring # used as key to a global `map` on the Go side
-    seekloc::Int
-    size::Int
-end
 
 mutable struct MmapStream # Mmap based
     mmap_ary::Vector{UInt8}
@@ -78,7 +71,7 @@ mutable struct HTTPStream
     end
 end
 
-const SourceStream = Union{MmapStream, HTTPStream, XRDStream}
+const SourceStream = Union{MmapStream, HTTPStream}
 
 function Base.read(fobj::SourceStream, ::Type{T}) where T
     return only(reinterpret(T, read(fobj, sizeof(T))))
@@ -123,50 +116,4 @@ end
 
 function Base.read(fobj::SourceStream)
     read(fobj, fobj.size - fobj.seekloc + 1)
-end
-
-function XRDStream(urlbase::AbstractString, filepath::AbstractString, username::AbstractString)
-    file_id = @ccall xrootdgo.Open(urlbase::Cstring, filepath::Cstring, username::Cstring)::Cstring
-    # file_id = @threadcall((:Open, xrootdgo), Cstring, (Cstring, Cstring, Cstring), urlbase, filepath, username)
-    size = @ccall xrootdgo.Size(file_id::Cstring)::Int
-    XRDStream(file_id, 0, size)
-end
-
-function Base.close(fobj::XRDStream)
-    xrootdgo.Close(fobj.gofile_id)
-end
-
-function read_seek_nb(fobj::XRDStream, seek, nb)
-    buffer = Vector{UInt8}(undef, nb)
-    @threadcall((:ReadAt, xrootdgo), Cvoid, (Ptr{UInt8}, Cstring, Clong, Clong), buffer, fobj.gofile_id, nb, seek)
-    # @ccall xrootdgo.ReadAt(buffer::Ptr{UInt8}, 
-    #                   fobj.gofile_id::Cstring, nb::Clong, seek::Clong)::Cvoid
-    return buffer
-end
-function _read!(ptr, fobj, nb, seekloc)
-    @ccall xrootdgo.ReadAt(ptr::Ptr{UInt8}, 
-                      fobj.gofile_id::Cstring, nb::Clong, seekloc::Clong)::Cvoid
-end
-
-function _read!(ptr, fobj, nb)
-    _read!(ptr, fobj, nb, fobj.seekloc)
-end
-
-function Base.read(fobj::XRDStream, ::Type{T}) where T
-    @debug @show T, sizeof(T)
-    nb = sizeof(T)
-    output = Ref{T}()
-    tko = Base.@_gc_preserve_begin output
-    po = Ptr{UInt8}(pointer_from_objref(output))
-    _read!(po, fobj, nb, fobj.seekloc)
-    Base.@_gc_preserve_end tko
-    fobj.seekloc += nb
-    return output[]
-end
-
-function Base.read(fobj::XRDStream, nb::Integer)
-    buffer = Vector{UInt8}(undef, nb)
-    GC.@preserve buffer _read!(buffer, fobj, nb, fobj.seekloc)
-    fobj.seekloc += nb
-    return buffer
 end
