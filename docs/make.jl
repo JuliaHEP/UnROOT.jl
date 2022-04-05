5c16dafe0cd3845b2d97b42d1a86efeb9c4bdab7
Tamas Gal <himself@tamasgal.com>
2022-04-05 20:56:40 +0200

    Fix URLs

--- docs/make.jl
+++ docs/make.jl
@@ -17,7 +17,7 @@ makedocs(;
         "For Contributors" => "devdocs.md",
         "APIs" => "internalapis.md",
     ],
-    repo="https://github.com/tamasgal/UnROOT.jl/blob/{commit}{path}#L{line}",
+    repo="https://github.com/JuliaHEP/UnROOT.jl/blob/{commit}{path}#L{line}",
     sitename="UnROOT.jl",
     authors="Tamas Gal and contributors",
 )
