26780eb2beeaae69037e2a87d24af0b39fa49384
Jerry Ling <proton@jling.dev>
2022-04-29 10:05:25 -0400

    fix C-stype array

--- src/root.jl
+++ src/root.jl
@@ -393,16 +393,12 @@ function auto_T_JaggT(f::ROOTFile, branch; customstructs::Dict{String, Type})
         end
     else
         _type = primitivetype(leaf)
-        if leaf.fLen > 1
-            _type = SVector{_type, 9}
-            _jaggtype = Nooffsetjagg
-
-            return _type, _jaggtype
+        if leaf.fLen > 1 # treat NTuple as Nojagg since size is static
+            _type = FixLenVector{Int(leaf.fLen), _type}
+            return _type, Nojagg
         end
-        _type = primitivetype(leaf)
         _type = _jaggtype === Nojagg ? _type : Vector{_type}
     end
-
     return _type, _jaggtype
 end
 
