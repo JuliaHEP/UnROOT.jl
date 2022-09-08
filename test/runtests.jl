e2bfc3865a2f8da274a9518d4fde78ac327fc8ad
Jerry Ling <proton@jling.dev>
2022-09-08 10:46:44 -0400

    put back network test

--- test/runtests.jl
+++ test/runtests.jl
@@ -737,16 +737,16 @@ end
     @test length(UnROOT.basketarray(t.b1, 1)) == 1228
 end
 
-# @testset "SourceStream remote" begin
-#     r = ROOTFile("root://eospublic.cern.ch//eos/root-eos/cms_opendata_2012_nanoaod/Run2012B_DoubleMuParked.root")
-#     @test r["Events"].fEntries == 29308627
-#     show(devnull, r) # test display
+@testset "SourceStream remote" begin
+    r = ROOTFile("root://eospublic.cern.ch//eos/root-eos/cms_opendata_2012_nanoaod/Run2012B_DoubleMuParked.root")
+    @test r["Events"].fEntries == 29308627
+    show(devnull, r) # test display
 
-#     t = LazyTree("https://scikit-hep.org/uproot3/examples/Zmumu.root", "events")
-#     @test t.eta1[1] ≈ -1.21769
-#     @test t.eta1[end] ≈ -1.57044
-#     show(devnull, t) # test display
-# end
+    t = LazyTree("https://scikit-hep.org/uproot3/examples/Zmumu.root", "events")
+    @test t.eta1[1] ≈ -1.21769
+    @test t.eta1[end] ≈ -1.57044
+    show(devnull, t) # test display
+end
 
 @testset "Cluster ranges" begin
     t = LazyTree(UnROOT.samplefile("tree_with_clusters.root"),"t1");
