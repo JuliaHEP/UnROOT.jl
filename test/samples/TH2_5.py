e5bd9705a46fdfc8fa3ebc2487c1e0b183aa813d
Yuan-Ru Lin <yuanruleonlin@gmail.com>
2023-02-17 01:41:04 +0900

    Reduce the TH2D and add value tests

--- test/samples/TH2_5.py
+++ test/samples/TH2_5.py
@@ -1,6 +1,6 @@
 import ROOT as r
 f = r.TFile("TH2_5.root", "recreate")
-th2f = r.TH2F("myTH2F", "", 10, -2, 2, 10, -2, 2)
+th2f = r.TH2F("myTH2F", "", 4, -2, 2, 4, -2, 2)
 for x,y,w in [
         [-1.5, -1.5, 20.0],
         [+1.5, +1.5, 1.0],
