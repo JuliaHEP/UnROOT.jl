62d9acde7bb391c88c8fde9ce515579387d56f2e
Jerry Ling <proton@jling.dev>
2023-02-25 21:01:06 -0500

    update

--- test/samples/RNTuple/rntuple_split_3e4.C
+++ test/samples/RNTuple/rntuple_split_3e4.C
@@ -7,8 +7,8 @@ R__LOAD_LIBRARY(ROOTNTuple)
 using RNTupleModel = ROOT::Experimental::RNTupleModel;
 using RNTupleWriter = ROOT::Experimental::RNTupleWriter;
 
-void rntuple_splitint_3e4() {
-    std::string rootFileName{"test_ntuple_splitint_3e4.root"};
+void rntuple_split_3e4() {
+    std::string rootFileName{"test_ntuple_split_3e4.root"};
     auto model = RNTupleModel::Create();
     auto splitint_field = model->MakeField<int32_t>("one_int32");
     auto splitint_field2 = model->MakeField<uint32_t>("two_uint32");
