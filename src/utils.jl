f15dc6dfb73bf9316df511db59671814b3bf3ac8
Tamas Gal <himself@tamasgal.com>
2023-03-15 14:24:47 +0100

    Preliminary fix with some mysteries about fID=-1

--- src/utils.jl
+++ src/utils.jl
@@ -58,11 +58,17 @@ struct Offsetjaggjagg  <:JaggType  end
 
 function JaggType(f, branch, leaf)
     # https://github.com/scikit-hep/uproot3/blob/54f5151fb7c686c3a161fbe44b9f299e482f346b/uproot3/interp/auto.py#L144
+
     try
-        streamer = streamerfor(f, branch.fClassName).streamer.fElements.elements[branch.fID + 1]
+        fID = branch.fID
+        if fID == -1 # TODO: unclear what -1 means, for now we treat it as "0"
+            fID = 0
+        end
+        streamer = streamerfor(f, branch.fClassName).streamer.fElements.elements[fID + 1]  # one-based indexing in Julia
         (streamer.fSTLtype == Const.kSTLvector) && return Offsetjagg
     catch
     end
+
     (match(r"\[.*\]", leaf.fTitle) !== nothing) && return Nooffsetjagg
     leaf isa TLeafElement && leaf.fLenType==0 && return Offsetjagg
     !hasproperty(branch, :fClassName) && return Nojagg
