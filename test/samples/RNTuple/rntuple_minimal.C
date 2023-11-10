9e30d59022d4caf160403776175871696f3c69d7
Moelf <proton@jling.dev>
2023-11-10 22:34:02 +0100

    restore file

--- test/samples/RNTuple/rntuple_minimal.C
+++ test/samples/RNTuple/rntuple_minimal.C
@@ -11,23 +11,21 @@ using RNTupleWriteOptions = ROOT::Experimental::RNTupleWriteOptions;
 void rntuple_minimal() {
   std::string rootFileName1{"test_ntuple_min1.root"};
   auto model1 = RNTupleModel::Create();
-  auto field1 = model1->MakeField<int>("one_int");
+  auto field1 = model1->MakeField<uint32_t>("one_uint");
   auto writeOptions = RNTupleWriteOptions();
   writeOptions.SetCompression(0);
   auto ntuple1 =
       RNTupleWriter::Recreate(std::move(model1), "ntuple", rootFileName1, writeOptions);
-  // 0xcccccc
-  *field1 = 13421772;
-  ntuple1->Fill();
+  // 0xcccccccc
+  *field1 = 3435973836;
   ntuple1->Fill();
 
   std::string rootFileName2{"test_ntuple_min2.root"};
   auto model2 = RNTupleModel::Create();
-  auto field2 = model2->MakeField<int>("one_int");
+  auto field2 = model2->MakeField<uint32_t>("one_uint");
   auto ntuple2 =
       RNTupleWriter::Recreate(std::move(model2), "ntuple", rootFileName2, writeOptions);
       // 0xeeeeee
-  *field2 = 15658734;
-  ntuple2->Fill();
+  *field2 = 4008636142;
   ntuple2->Fill();
 }
