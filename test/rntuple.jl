ef161336c741e99a924ed17f696ef479e8bf51b9
Jerry Ling <proton@jling.dev>
2024-04-02 08:30:01 -0400

    Update rntuple.jl

--- test/rntuple.jl
+++ test/rntuple.jl
@@ -230,10 +230,10 @@ end
 @testset "Footer extension header links backfill" begin
     f1 = UnROOT.samplefile("RNTuple/test_ntuple_extension_columns.root")
     df = LazyTree(f1, "EventData", ["HLT_AntiKt4EMPFlowJets_subresjesgscIS_ftf_TLAAux::fastDIPS20211215_pb", "HLT_AntiKt4EMPFlowJets_subresjesgscIS_ftf_TLAAux:"])
-    pbs = collect(data22_aod_rnt.var"HLT_AntiKt4EMPFlowJets_subresjesgscIS_ftf_TLAAux::fastDIPS20211215_pb")
+    pbs = collect(df.var"HLT_AntiKt4EMPFlowJets_subresjesgscIS_ftf_TLAAux::fastDIPS20211215_pb")
     @test length(pbs) == 40
     @test findfirst(!isempty, pbs) == 37
-    jets = collect(data22_aod_rnt.var"HLT_AntiKt4EMPFlowJets_subresjesgscIS_ftf_TLAAux:")
+    jets = collect(df.var"HLT_AntiKt4EMPFlowJets_subresjesgscIS_ftf_TLAAux:")
 
     @test length.(pbs) == length.(jets)
 end
