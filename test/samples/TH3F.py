69b914bbd6d8e88d2829b3c89aefd99e7cccc51f
Tamas Gal <himself@tamasgal.com>
2024-05-06 20:42:59 +0200

    Improve TH3F sample

--- test/samples/TH3F.py
+++ test/samples/TH3F.py
@@ -1,16 +1,24 @@
+import random
 import ROOT
 
 # Create a ROOT file to store histograms
 file = ROOT.TFile("TH3F.root", "RECREATE")
 
 # Create a 3D histogram
-histogram = ROOT.TH3F("histogram", "Example 3D Histogram;X Axis;Y Axis;Z Axis", 50, -5, 5, 50, -5, 5, 50, -5, 5)
+histogram = ROOT.TH3F(
+        "histogram",
+        "Example 3D Histogram;X Axis;Y Axis;Z Axis",
+        10, -1, 1,
+        20, -2, 2,
+        30, -3, 3)
+
+random.seed(42)
 
 # Fill the histogram with some random data
 for i in range(1, 10000):
-    x = 1./i
-    y = 1./(2*i)
-    z = 1./(3*i)
+    x = random.random() * 2 - 1
+    y = random.random() * 4 - 2
+    z = random.random() * 6 - 3
     histogram.Fill(x, y, z)
 
 # Write the histogram to the ROOT file
