0a098dc8760dc66e34a19d85fe61cf72b2c3e23f
Tamas Gal <himself@tamasgal.com>
2024-05-07 01:04:59 +0200

    Add tests for TH3F

--- test/histograms.jl
+++ test/histograms.jl
@@ -78,4 +78,42 @@ using FHist
     binlabels = ["Root", "Weight", "Preselection", "SelectGenPart", "GoodRunsList", "EventFilters", "SelectLeptons", "SelectJets", "Trigger", "ObjectsSelection", "SSPreselection", "NjetGeq4", "AK4CategTagHiggsJets", "AK4CategTagVBSJets", "AK4CategChannels", "AK4CategPresel"]
     @test f["AK4CategPresel_cutflow"][:fXaxis_fModLabs].objects == binlabels
     close(f)
+
+    f = UnROOT.samplefile("TH3F.root")
+    h = f["histogram"]
+    @test 45699.0 ≈ h[:fTsumw]
+    @test 45699.0 ≈ h[:fTsumw2]
+    @test 133.84083137224275 ≈ h[:fTsumwx]
+    @test 15255.146094082058 ≈ h[:fTsumwx2]
+    @test 489.6839597196363 ≈ h[:fTsumwy]
+    @test 60853.5600420268 ≈ h[:fTsumwy2]
+    @test 206.84178180322968 ≈ h[:fTsumwxy]
+    @test -424.5948793991111 ≈ h[:fTsumwz]
+    @test 137144.65414345643 ≈ h[:fTsumwz2]
+    @test -233.101696139249 ≈ h[:fTsumwxz]
+    @test -383.8438776730445 ≈ h[:fTsumwyz]
+    @test 2 == h[:fStatOverflows]
+    @test -1111 == h[:fMinimum]
+    @test -1111 == h[:fMaximum]
+    @test 99999.0 == h[:fEntries]
+    @test 0.0 == h[:fNormFactor]
+
+    @test 10 == h[:fXaxis_fNbins]
+    @test -1.0 == h[:fXaxis_fXmin]
+    @test 1.0 == h[:fXaxis_fXmax]
+    @test 0 == h[:fXaxis_fFirst]
+    @test 0 == h[:fXaxis_fLast]
+
+    @test 20 == h[:fYaxis_fNbins]
+    @test -2.0 == h[:fYaxis_fXmin]
+    @test 2.0 == h[:fYaxis_fXmax]
+    @test 0 == h[:fYaxis_fFirst]
+    @test 0 == h[:fYaxis_fLast]
+
+    @test 30 == h[:fZaxis_fNbins]
+    @test -3.0 == h[:fZaxis_fXmin]
+    @test 3.0 == h[:fZaxis_fXmax]
+    @test 0 == h[:fZaxis_fFirst]
+    @test 0 == h[:fZaxis_fLast]
+    close(f)
 end
