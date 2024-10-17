f7a699518e09e87561c308d4c24b1f04346a0a68
Moelf <proton@jling.dev>
2024-10-18 00:15:34 +0200

    more tests
    more tests
    
    fix
    
    print everything in PyROOT
    
    add coverage
    
    add coverage

--- test/RNTupleWriting/test1.py
+++ test/RNTupleWriting/test1.py
@@ -1,9 +1,15 @@
 import ROOT
 import sys
 
+Nitems = 10
+data = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+
 df  =  ROOT.RDataFrame("myntuple", sys.argv[1])
-if len(list(df.GetColumnNames())) != 4:
+
+if list(df.Take['std::int32_t']("x_Int32")) != data:
+    sys.exit(1)
+if list(df.Take['std::int64_t']("x_Int64")) != data:
     sys.exit(1)
 
-if list(df.Take['std::int32_t']("x3")) != [5, 6, 7, 8, 9, 10, 11, 12, 13, 14]:
-    sys.exit(1)
+for n in df.GetColumnNames():
+    df.Display(n).Print()
